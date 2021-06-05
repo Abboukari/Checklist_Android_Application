b10beca6576710f2eb7c834be1129ca2706616db
SupeRenee <reneeclerx@gmail.com>
2021-06-05 08:18:39 +0200

    1 database instead of 3, everything is  in UserDatabase

--- app/src/main/java/com/example/apeptodaygroep4/Database/DataBaseLabel.java
+++
@@ -1,4 +0,0 @@
-package com.example.apeptodaygroep4.Database;
-
-public abstract class DataBaseLabel {
-}
--- app/src/main/java/com/example/apeptodaygroep4/Database/UserDatabase.java
+++ app/src/main/java/com/example/apeptodaygroep4/Database/UserDatabase.java
@@ -4,9 +4,11 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 import com.example.apeptodaygroep4.Dao.UserDao;
+import com.example.apeptodaygroep4.Models.LabelDb;
+import com.example.apeptodaygroep4.Models.Task;
 import com.example.apeptodaygroep4.Models.User;
 
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Task.class, LabelDb.class}, version = 1, exportSchema = false)
 public abstract class UserDatabase extends RoomDatabase {
 
     public abstract UserDao getUserDao();
