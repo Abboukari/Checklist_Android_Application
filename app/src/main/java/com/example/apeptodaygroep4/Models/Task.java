0c2d64c559773b54e56bf68bddce927a27fc0de3
SupeRenee <reneeclerx@gmail.com>
2021-06-08 20:31:02 +0200

    met stuk

--- app/src/main/java/com/example/apeptodaygroep4/Models/Task.java
+++ app/src/main/java/com/example/apeptodaygroep4/Models/Task.java
@@ -3,12 +3,17 @@ package com.example.apeptodaygroep4.Models;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Entity
+@Entity(foreignKeys = {@ForeignKey(
+        entity = User.class,
+        parentColumns = "id",
+        childColumns = "uIdUser"),
+        })
 public class Task {
     @PrimaryKey int uIdTask;
     @ColumnInfo int uIdUser;
