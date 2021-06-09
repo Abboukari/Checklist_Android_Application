b7c9e3bdb3fb96d455c45d8c0bce5fab17e1befd
SupeRenee <reneeclerx@gmail.com>
2021-06-09 07:30:23 +0200

    voorbereiding opmaak recycleview

---
+++ app/src/main/java/com/example/apeptodaygroep4/ui/tasks/TaskFragment.java
@@ -0,0 +1,11 @@
+package com.example.apeptodaygroep4.ui.tasks;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.apeptodaygroep4.R;
+
+public class TaskFragment extends Fragment{
+    public TaskFragment(){
+        super(R.layout.activity_home);
+    }
+}
