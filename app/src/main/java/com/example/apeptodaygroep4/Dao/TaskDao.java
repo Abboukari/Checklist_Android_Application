6035eab4d42c9d4edcd77e564b2335bd42f51482
Abboukari <71227631+Abboukari@users.noreply.github.com>
2021-06-12 12:05:20 +0200

    Delete method finished

--- app/src/main/java/com/example/apeptodaygroep4/Dao/TaskDao.java
+++ app/src/main/java/com/example/apeptodaygroep4/Dao/TaskDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.apeptodaygroep4.Models.Task;
 
@@ -17,10 +18,15 @@ public interface TaskDao {
     @Query("SELECT Title, uIdUser FROM Task WHERE uIdUser = :userId")
     List<Task> getTilteTasks(int userId);
 
+    @Query("SELECT * FROM Task WHERE uIdUser = :userId")
+    Task getTask(int userId);
 
     @Insert
     void addTask(Task task);
 
     @Delete
     void deleteTask(Task task);
+
+    @Update
+    void updateTask(Task task);
 }
